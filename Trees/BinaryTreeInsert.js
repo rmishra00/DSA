bfeffc676f7b06b3ae5652789f6aa8c85b6c8fed
rmishra00 <riya@nostacklabs.com>
2026-06-29 20:31:27 +0530

    Insert in bt

---
+++ Trees/BinaryTreeInsert.js
@@ -0,0 +1,48 @@
+class TreeNode{
+    constructor(val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+// const node1 = new TreeNode(10);
+// const node2 = new TreeNode(5);
+// const node3 = new TreeNode(15);
+// console.log(node1.val);
+
+// const root = new TreeNode(10);
+// root.right = new TreeNode(15);
+// root.left = new TreeNode(5);
+// root.left.left = new TreeNode(3);
+// root.right.right = new TreeNode(7);
+
+class BinaryTree{
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        const newNode = new TreeNode(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+        let current = this.root;
+        while(true){
+            if(value === current.val) return undefined;
+        }
+        if(value<current.val){
+            if(current.left === null){
+            current.next = newNode;
+            return this;
+        }
+                current = current.next;  
+        }else{
+            if(current.right === null){
+                current.right = newNode;
+                return this;
+            }
+            current = current.right;
+        }
+   
+    }
+}
